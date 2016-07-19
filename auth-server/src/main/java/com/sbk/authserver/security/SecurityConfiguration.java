3a34e1882223b6bfe77095e7c6cb10ee37effbd4
SergiySobolev <sergiy.sobolev@gmail.com>
2016-07-19 23:11:12 +0300

    security config to separate file

--- auth-server/src/main/java/com/sbk/authserver/security/SecurityConfiguration.java
+++ auth-server/src/main/java/com/sbk/authserver/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.sbk.authserver.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,53 +14,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
+@Order(-20)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("password")
-                .roles("USER");
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/webjars/**", "/images/**", "/oauth/uncache_approvals", "/oauth/cache_approvals");
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/webjars/**").permitAll()
-                .anyRequest().authenticated()
+                .formLogin().loginPage("/login").permitAll()
                 .and()
-                .formLogin()
+                .requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
                 .and()
-              //  .httpBasic()
-                .csrf().disable()
-//                .and().exceptionHandling()
-//                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-        ;
-//        http
-//                .authorizeRequests()
-//                .anyRequest().authenticated()
-//
-//                .and().formLogin().permitAll()
-//
-//                .and().csrf().disable();
-//        http.httpBasic();
-
+                .authorizeRequests().anyRequest().authenticated();
     }
 }
